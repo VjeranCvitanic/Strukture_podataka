abec73a7ef85ab9a727822fa5daac452d3328b1a
Vjeran Cvitanić <92399175+VjeranCvitanic@users.noreply.github.com>
2021-10-28 22:07:26 +0200

    Update zadatak2.c
    Druga verzija drugog zadatka, prva je zakomentirana ispod.

--- zadatak2.c
+++ zadatak2.c
@@ -1,4 +1,257 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_LINE 100
+
+struct _Person;
+typedef struct _Person* Position;
+typedef struct _Person
+{
+	char name[MAX_LINE];
+	char surname[MAX_LINE];
+	int birthYear;
+	Position next;
+}Person;
+
+int PrependList(Position p, char* name, char* surname, int birthYear);
+Position CreatePerson(char* name, char* surname, int birthYear);
+void PrintList(Position p);
+int AppendList(Position p,char* name,char* surname,int birthYear);
+void insertAfter(Position one, Position toBeInserted);
+Position FindLast(Position p);
+Position FindBySurname(Position p, char* surname);
+Position FindBefore(Position p, Position target);
+int DeleteAfter(Position one);
+
+int main()
+{
+	Person Head = { .name = " ", .surname = " ", .birthYear = 0, .next = NULL };
+	Position p = NULL;
+	Position newPerson;
+	Position wantedSurname;
+	Position before;
+	p = &Head;
+	char name[MAX_LINE];
+	char surname[MAX_LINE];
+	int birthyear;
+	int br = 0;
+	int flag = 0;
+	
+	while (flag == 0)
+	{
+		printf("Menu:\nInput 1 for adding a person at the start\nInput 2 for adding a person at the end of the list\nInput 3 for printing the list\nInput 4 for finding a person by his/her surname\nInput 5 for deleting a person\nInput 6 for exiting\n");
+		scanf("%d", &br);
+		switch (br)
+		{
+			case 1:
+			{
+				printf("Enter name, surname and birthyear:\n");
+				scanf("%s %s %d", name, surname, &birthyear);
+				PrependList(p, name, surname, birthyear);
+				break;
+			}
+
+			case 2:
+			{
+				printf("Enter name, surname and birthyear:\n");
+				scanf("%s %s %d", name, surname, &birthyear);
+				AppendList(p, name, surname, birthyear);
+				break;
+			}
+
+			case 3:
+			{
+				printf("\n");
+				PrintList(p);
+				printf("\n");
+				break;
+			}
+
+			case 4:
+			{
+				printf("Enter the surname:\n");
+				scanf("%s", surname);
+				wantedSurname = FindBySurname(p, surname);
+				printf("The person is %s %s, born in %d.\n", wantedSurname->name, wantedSurname->surname, wantedSurname->birthYear);
+				break;
+			}
+
+			case 5:
+			{
+				printf("Enter the surname:\n");
+				scanf("%s", surname);
+				wantedSurname = FindBySurname(p, surname);
+				before = FindBefore(p, wantedSurname);
+				DeleteAfter(before);
+				break;
+			}
+
+			case 6:
+			{
+				flag = 1;
+				break;
+			}
+
+			default:
+			{
+				printf("Wrong input!\n");
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+Position CreatePerson(char* name, char* surname, int birthYear)
+{
+	Position newPerson = NULL;
+	newPerson = (Position)malloc(sizeof(Person));
+	if (!newPerson)
+	{
+		printf("Can't allocate memory!\n");
+		return NULL;
+	}
+	strcpy(newPerson->name, name);
+	strcpy(newPerson->surname, surname);
+	newPerson->birthYear = birthYear;
+	newPerson->next = NULL;
+	return newPerson;
+}
+
+int PrependList(Position p, char* name, char* surname, int birthYear)
+{
+	Position NewPerson = CreatePerson(name, surname, birthYear);
+	if (!NewPerson)
+	{
+		printf("Couldn't create a new person!\n");
+		return EXIT_FAILURE;
+	}
+	insertAfter(p, NewPerson);
+	return EXIT_SUCCESS;
+}
+
+void PrintList(Position p)
+{
+	Position temp = p->next;
+	while (temp)
+	{
+		printf("%s %s %d\n", temp->name, temp->surname, temp->birthYear);
+		temp = temp->next;
+	}
+}
+
+int AppendList(Position p, char* name, char* surname, int birthYear)
+{
+	Position newPerson = NULL;
+	Position temp = p;
+	newPerson = (Position)malloc(sizeof(Position));
+	if (!newPerson)
+	{
+		printf("Couldn't create a new person!\n");
+		return EXIT_FAILURE;
+	}
+	newPerson = CreatePerson(name, surname, birthYear);
+	while(temp->next)
+	{
+		temp = temp->next;
+	}
+	Position last = FindLast(temp);
+	insertAfter(last, newPerson);
+	return EXIT_SUCCESS;
+}
+
+void insertAfter(Position one, Position toBeInserted)
+{
+	toBeInserted->next = one->next;
+	one->next = toBeInserted;
+}
+
+Position FindLast(Position p)
+{
+	Position last = NULL;
+	while (p->next)
+	{
+		p = p->next;
+	}
+	last = p;
+	return last;
+}
+
+Position FindBySurname(Position p, char* surname)
+{
+	Position temp = p->next;
+	while (temp)
+	{
+		if (strcmp(temp->surname, surname) == 0) 
+		{
+			return temp;
+		}
+		temp = temp->next;
+	}
+	printf("Nema osobe prezimena: %c u listi.\n", *surname);
+	return NULL;
+}
+
+Position FindBefore(Position p, Position target)
+{
+	Position temp = p;
+	while ((temp->next) != target)
+	{
+		temp = temp->next;
+	}
+	return temp;
+}
+
+int DeleteAfter(Position one)
+{
+	Position a = one->next;
+	one->next = one->next->next;
+	free(a);
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//////////////////////////////////////////////////////////////*/
+
+/*#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -264,3 +517,4 @@ int unosInta()
 	unos = atoi(Unos);
 	return unos;
 }
+*/
